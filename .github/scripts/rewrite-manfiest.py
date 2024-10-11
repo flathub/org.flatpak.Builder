2b4ee3cfad57cb04f036df60a54dd871457e97c3
bbhtt <bbhtt.zn0i8@slmail.me>
2024-10-11 19:20:48 +0530

    Add workflow to automate releases from linter repo

---
+++ .github/scripts/rewrite-manfiest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import json
+import os
+
+SHA = os.environ["LINTER_SHA"]
+MANIFEST_NAME = "org.flatpak.Builder.json"
+
+with open(MANIFEST_NAME) as f:
+    manifest = json.load(f)
+
+for module in manifest["modules"]:
+    if isinstance(module, dict) and module.get("name") == "flatpak-builder-lint":
+        module["sources"] = [{"type": "git", "url": "https://github.com/flathub/flatpak-builder-lint", "commit": SHA}]
+        break
+
+with open(MANIFEST_NAME, "w") as f:
+    json.dump(manifest, f, indent=4)
+    f.write("\n")
