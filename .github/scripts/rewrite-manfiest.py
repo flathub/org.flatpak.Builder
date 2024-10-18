d87117375b6ddad17b47f19874aad13d33d61bea
bbhtt <bbhtt.zn0i8@slmail.me>
2024-10-18 08:29:48 +0530

    rewrite-manfiest: Add poor man's error handling

--- .github/scripts/rewrite-manfiest.py
+++ .github/scripts/rewrite-manfiest.py
@@ -4,6 +4,10 @@ import json
 import os
 
 SHA = os.environ["LINTER_SHA"]
+
+if not (SHA and len(SHA) == 40):
+    raise Exception("Invalid SHA")
+
 MANIFEST_NAME = "org.flatpak.Builder.json"
 
 with open(MANIFEST_NAME) as f:
@@ -11,7 +15,13 @@ with open(MANIFEST_NAME) as f:
 
 for module in manifest["modules"]:
     if isinstance(module, dict) and module.get("name") == "flatpak-builder-lint":
-        module["sources"] = [{"type": "git", "url": "https://github.com/flathub/flatpak-builder-lint", "commit": SHA}]
+        module["sources"] = [
+            {
+                "type": "git",
+                "url": "https://github.com/flathub/flatpak-builder-lint",
+                "commit": SHA,
+            }
+        ]
         break
 
 with open(MANIFEST_NAME, "w") as f:
